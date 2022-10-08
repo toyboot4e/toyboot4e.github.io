2536f6311cea204223381591b2db9a3aa0efe7ec
toyboot4e <toyboot4e@gmail.com>
2022-10-08 14:41:09 +0900

    add `prism.js` support

--- build.el
+++ build.el
@@ -1,8 +1,9 @@
 ;; Build script
 ;;
 ;; Thanks:
-;; - https://systemcrafters.net/publishing-websites-with-org-mode/building-the-site/
-;; - https://taingram.org/blog/org-mode-blog.html
+;; - `https://systemcrafters.net/publishing-websites-with-org-mode/building-the-site/'
+;; - `https://taingram.org/blog/org-mode-blog.html'
+;; - `https://www.roygbyte.com/add_syntax_highlighting_to_an_org_publish_project.html'
 
 (setq make-backup-files nil)
 
@@ -34,7 +35,8 @@
          :base-extension "org"
          :publishing-directory "./out"
          :recursive t
-         :publishing-function org-html-publish-to-html
+         ;; Custom function defined below
+         :publishing-function my-org-html-publish-to-html
          :section-numbers t
          :with-author nil
          :with-creator nil
@@ -56,7 +58,7 @@
 ;; Remove `validate' link at the bottom
 (setq org-html-validation-link nil)
 
-;; Surround title text with `<a>' tag link
+;; Add link to each heading
 (setq org-html-self-link-headlines t)
 
 ;; Use custom CSS
@@ -83,10 +85,61 @@
                              (insert-file-contents "src/style/postamble.html")
                              (buffer-string)))
 
-;; Use custom export functions
-(defun ox-mrkup-filter-code
-        (text back-end info)
-    (format "<code>%s</code>" text))
+;;; Backend
+(defun roygbyte/org-html-src-block (src-block _contents info)
+  "Transcode a SRC-BLOCK element from Org to HTML.
+  CONTENTS holds the contents of the item.  INFO is a plist holding
+  contextual information."
+  (if (org-export-read-attribute :attr_html src-block :textarea)
+      (org-html--textarea-block src-block)
+    (let* ((lang (org-element-property :language src-block))
+           (code (org-html-format-code src-block info))
+           (label (let ((lbl (org-html--reference src-block info t)))
+                    (if lbl (format " id=\"%s\"" lbl) "")))
+           (klipsify  (and  (plist-get info :html-klipsify-src)
+                            (member lang '("javascript" "js"
+                                           "ruby" "scheme" "clojure" "php" "html")))))
+      (if (not lang) (format "<pre class=\"example\"%s>\n%s</pre>" label code)
+        (format "<div class=\"org-src-container\">\n%s%s\n</div>"
+                ;; Build caption.
+                (let ((caption (org-export-get-caption src-block)))
+                  (if (not caption) ""
+                    (let ((listing-number
+                           (format
+                            "<span class=\"listing-number\">%s </span>"
+                            (format
+                             (org-html--translate "Listing %d:" info)
+                             (org-export-get-ordinal
+                              src-block info nil #'org-html--has-caption-p)))))
+                      (format "<label class=\"org-src-name\">%s%s</label>"
+                              listing-number
+                              (org-trim (org-export-data caption info))))))
+                ;; Contents.
+                ;; Changed HTML template to work with Prism.
+                (if klipsify
+                    (format "<pre><code class=\"src language-%s\"%s%s>%s</code></pre>"
+                            lang
+                            label
+                            (if (string= lang "html")
+                                " data-editor-type=\"html\""
+                              "")
+                            code)
+                  (format "<pre><code class=\"src language-%s\"%s>%s</code></pre>"
+                          lang label code)))))))
+
+(org-export-define-derived-backend 'my-site-html
+                                   'html ;; 'slimhtml
+                                   :translate-alist
+                                   '((src-block . roygbyte/org-html-src-block)))
+
+(defun my-org-html-publish-to-html (plist filename pub-dir)
+  "Publish an org file to HTML, using the FILENAME as the output directory."
+  (org-publish-org-to 'my-site-html filename
+                      (concat (when (> (length org-html-extension) 0) ".")
+                              (or (plist-get plist :html-extension)
+                                  org-html-extension
+                                  "html"))
+                      plist pub-dir))
 
 ;;; Build
 (org-publish-all t)
