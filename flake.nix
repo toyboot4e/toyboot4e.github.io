3774110fd735e97aacb7c3ded9c2642e272fab50
toyboot4e <toyboot4e@gmail.com>
2025-05-10 08:08:30 +0900

    add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A basic flake for my devlog";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          devlog = pkgs.stdenvNoCC.mkDerivation {
+            name = "devlog";
+            src = ./.;
+            nativeBuildInputs = with pkgs; [
+              (emacs.pkgs.withPackages (epkgs: with epkgs; [ seq esxml ]))
+              nodePackages.prettier
+            ];
+            buildPhase = ''
+              export HOME="$(mktemp -d)"
+              emacs -Q --script "./build.el" -- "--release"
+              prettier --print-width 100 --write out/*.html out/diary/*.html
+            '';
+            installPhase = ''
+              mkdir -p $out
+              mv out $out/site
+            '';
+          };
+        };
+      }
+    );
+}
